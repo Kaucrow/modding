74db2cb0a3d4955f1e4e9435dd24a5ed80b6eebe
Kaucrow <kaucrow@protonmail.com>
2025-04-29 11:31:29 -0400

    first commit

---
+++ TemplateMod/TemplateMod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using RWCustom;
+using BepInEx;
+using Debug = UnityEngine.Debug;
+using MonoMod.RuntimeDetour;  // Critical for 'On' hooks
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace TemplateMod;
+
+[BepInPlugin("kaucrow.myrainworldmod", "My Rain World Mod", "1.0.0")]
+public partial class TemplateMod : BaseUnityPlugin
+{
+    private void OnEnable()
+    {
+        On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+    }
+
+    private bool IsInit;
+    private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+        if (IsInit) return;
+
+        try
+        {
+            IsInit = true;
+
+            //Your hooks go here
+            On.Player.Update += PlayerOnUpdate;
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+        }
+    }
+
+}
