b494aa88b980b9a7400c272336d23fdaccfb3ae0
kaucrow <kaucrow@protonmail.com>
2025-05-02 18:06:58 -0400

    feat: Lantern Mice managed to make their single braincell recognize items

--- rainworld/micepups/LanternMouse.cs
+++ rainworld/micepups/LanternMouse.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
+using HarmonyLib;
 using IL.Stove.Sample.Ownership;
 using System.ComponentModel;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib.Tools;
+using BepInEx.Logging;
+using static StaticWorld;
+using System.CodeDom;
 
 namespace MicePupsMod;
 
@@ -40,11 +48,6 @@ public partial class MicePupsMod
             scale = 1f,
         };
         rCam.ReturnFContainer("Foreground").AddChild(sLeaser.sprites[markGlowSprite]); 
-
-        Debug.Log($"New LanternMouse leaser created with {sLeaser.sprites.Length} sprites");
-        Debug.Log($"The mark's position is: {sLeaser.sprites[markSprite].GetPosition()}");
-        Logger.LogInfo($"New LanternMouse leaser created with {sLeaser.sprites.Length} sprites");
-        Logger.LogInfo($"The mark's position is: {sLeaser.sprites[markSprite].GetPosition()}");
     }
 
     private void OnDraw(
@@ -76,7 +79,7 @@ public partial class MicePupsMod
         sLeaser.sprites[markGlowSprite].x = headPos.x - camPos.x; // <- CRUCIAL: Subtract camera position
         sLeaser.sprites[markGlowSprite].y = (headPos.y - camPos.y) + 50f; // 15 pixels above head
 
-        // 2. THEN call the original method (including base.DrawSprites)
+        // 2. THEN call the original method (including __instance.DrawSprites)
         orig(self, sLeaser, rCam, timeStacker, camPos);
 
         // 3. Optional post-processing
@@ -84,7 +87,131 @@ public partial class MicePupsMod
         {
             sLeaser.sprites[markSprite].MoveToFront();
         }
+    }
 
-        Logger.LogInfo($"The mark's new position is: {sLeaser.sprites[markSprite].GetPosition()}");
+    private void OnInitWorldRelationships(On.StaticWorld.orig_InitStaticWorldRelationships orig) {
+        orig();
+
+         var mouseTemplate = StaticWorld.GetCreatureTemplate(CreatureTemplate.Type.LanternMouse);
+        if (mouseTemplate != null)
+        {
+            // Modify relationship with other Lantern Mice
+            mouseTemplate.relationships[(int)CreatureTemplate.Type.Slugcat] = 
+                new CreatureTemplate.Relationship(CreatureTemplate.Relationship.Type.Ignores, 0.0f);
+
+        }
+    }
+
+    private void OnItemTrackerUpdate(On.ItemTracker.orig_Update orig, ItemTracker self) {
+        orig(self);
+
+        Logger.LogDebug($"Item tracker updated for creature: {self.AI.creature}");
+        Logger.LogDebug($"Item count: {self.ItemCount}");
+    }
+
+    private void OnAbstractCreatureInitiateAI(On.AbstractCreature.orig_InitiateAI orig, AbstractCreature self) {
+        orig(self);
+
+        if (self.creatureTemplate.TopAncestor().type == CreatureTemplate.Type.LanternMouse)
+        {
+            self.abstractAI.RealAI = new MouseAIWithItemTracker(self, self.world);
+        }
     }
 }
+
+[HarmonyPatch(typeof(ArtificialIntelligence))]
+[HarmonyPatch(typeof(ArtificialIntelligence), "VisualContact", new[] { typeof(BodyChunk) })]
+class VisualContact_BodyChunk_Patch
+{
+    [HarmonyPostfix]
+    static void Postfix(ArtificialIntelligence __instance, BodyChunk chunk, bool __result)
+    {
+        Console.WriteLine($"[VisualContact] Creature: {__instance.creature}, " +
+            $"Target: {chunk.owner}, " +
+            $"Result: {__result}");
+    }
+}
+
+public class MouseAIWithItemTracker : MouseAI, IUseItemTracker
+{
+    public MouseAIWithItemTracker(AbstractCreature creature, World world) 
+        : base(creature, world) // Pass parameters to base class
+    {
+    }
+
+    public bool TrackItem(AbstractPhysicalObject obj)
+    {
+        return obj.realizedObject == null || !(obj.realizedObject is Weapon) || (obj.realizedObject as Weapon).mode != Weapon.Mode.StuckInWall;
+    }
+
+    public void SeeThrownWeapon(PhysicalObject obj, Creature thrower)
+    {
+        if (base.tracker.RepresentationForObject(thrower, false) == null)
+        {
+            base.noiseTracker.mysteriousNoises += 20f;
+            base.noiseTracker.mysteriousNoiseCounter = 200;
+        }
+    }
+}
+
+[HarmonyPatch(typeof(MouseAI))]
+[HarmonyPatch(MethodType.Constructor)]
+[HarmonyPatch(new Type[] { typeof(AbstractCreature), typeof(World) })]
+public static class MouseAI_Constructor_Patch
+{
+    [HarmonyPrefix]
+    static bool Prefix(MouseAI __instance, AbstractCreature creature, World world)
+    {
+        Console.WriteLine("Called Harmony MouseAI constructor");
+
+        if (!(creature.realizedCreature is LanternMouse)) return true;
+
+        // Properly initialize base class
+        typeof(ArtificialIntelligence)
+            .GetConstructor(new[] { typeof(AbstractCreature), typeof(World) })
+            .Invoke(__instance, new object[] { creature, world });
+
+        __instance.mouse = creature.realizedCreature as LanternMouse;
+		__instance.mouse.AI = __instance;
+		__instance.AddModule(new StandardPather(__instance, world, creature));
+		__instance.AddModule(new Tracker(__instance, 10, 10, 450, 0.5f, 5, 5, 10, false));
+		__instance.AddModule(new ThreatTracker(__instance, 10));
+		__instance.AddModule(new RainTracker(__instance));
+		__instance.AddModule(new DenFinder(__instance, creature));
+		__instance.AddModule(new UtilityComparer(__instance));
+		__instance.AddModule(new RelationshipTracker(__instance, __instance.tracker));
+        __instance.AddModule(new FriendTracker(__instance));
+        __instance.AddModule(new ItemTracker(__instance, 10, 10, -1, -1, true));
+		__instance.utilityComparer.AddComparedModule(__instance.threatTracker, null, 1f, 1.1f);
+		__instance.utilityComparer.AddComparedModule(__instance.rainTracker, null, 1f, 1.1f);
+		__instance.behavior = MouseAI.Behavior.Idle;
+
+        return false;
+    }
+}
+
+/*
+[HarmonyPatch(typeof(SLOracleBehaviorHasMark.MoonConversation))]
+[HarmonyPatch("AddEvents")]
+public static class MoonDialogue_Patch
+{
+    private static void Postfix(SLOracleBehaviorHasMark.MoonConversation __instance)
+    {
+        // Only modify if this is the "case 5" dialogue
+        if (__instance.State.neuronsLeft == 5)
+        {
+            var events = __instance.events;
+
+            events.Clear();
+
+            events.Add(new Conversation.TextEvent(__instance, 0, "Lirilí Larilà.", 0));
+            events.Add(new Conversation.TextEvent(__instance, 0, "Elefante nel deserto che cammina qua e là.", 0));
+
+            if (__instance.State.playerEncounters > 0)
+            {
+                events.Add(new Conversation.TextEvent(__instance, 0, "Con la sua conchiglia y un orologio che fa tic tac, le spin del cactus mi fanno un attacco flashback.", 10));
+            }
+        }
+    }
+}
+*/
--- rainworld/micepups/MicePupsMod.cs
+++ rainworld/micepups/MicePupsMod.cs
@@ -9,7 +9,13 @@ using MonoMod.RuntimeDetour;
 using System.Data.SqlClient;
 using UnityEngine.Rendering;
 using System.Runtime.Serialization;
-using IL.Menu.Remix.MixedUI;  // Critical for 'On' hooks
+using IL.Menu.Remix.MixedUI;
+using HarmonyLib;
+using System.ComponentModel;
+using Menu.Remix.MixedUI;
+using On.Menu.Remix.MixedUI;
+using System.Runtime.Remoting.Messaging;
+using System.Xml;  // Critical for 'On' hooks
 
 #pragma warning disable CS0618
 
@@ -21,9 +27,21 @@ namespace MicePupsMod;
 [BepInPlugin("kaucrow.micepups", "Mice Pups", "1.0.0")]
 public partial class MicePupsMod : BaseUnityPlugin
 {
+    public static MicePupsMod Instance { get; private set; }
+    private Harmony _harmony;
+
     private void OnEnable()
     {
+        Instance = this;
+
+        _harmony = new Harmony("com.kaucrow.micepups");
+
         On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+    }
+
+    private void OnDisable()
+    {
+        _harmony?.UnpatchAll();
     }
 
     private bool IsInit;
@@ -36,6 +54,9 @@ public partial class MicePupsMod : BaseUnityPlugin
         {
             IsInit = true;
 
+            // Apply Harmony patches
+            _harmony.PatchAll();
+
             // Hooks go here
             On.Player.Update += PlayerOnUpdate;
             //On.Player.Die += PlayerOnDie;
@@ -43,6 +64,12 @@ public partial class MicePupsMod : BaseUnityPlugin
 
             On.MouseGraphics.InitiateSprites += MouseAddMark;
             On.MouseGraphics.DrawSprites += OnDraw;
+
+            On.StaticWorld.InitStaticWorldRelationships += OnInitWorldRelationships;
+
+            On.ItemTracker.Update += OnItemTrackerUpdate;
+
+            On.AbstractCreature.InitiateAI += OnAbstractCreatureInitiateAI;
         }
         catch (Exception ex)
         {
